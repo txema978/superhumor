7e53590fa8f73ddb268ab1e19fd258985e8a46be
txema978 <txema978@gmail.com>
2025-08-15 20:27:41 +0200

    Add files via upload

---
+++ service-worker-v32.js
@@ -0,0 +1,22 @@
+const CACHE = "superhumor-cache-v32";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.destination === "document") {
+    e.respondWith(fetch(e.request).then(res => { const clone=res.clone(); caches.open(CACHE).then(cache=>cache.put(e.request, clone)); return res; }).catch(()=>caches.match(e.request)));
+    return;
+  }
+  if (e.request.method === "GET" && url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((res) => { const clone = res.clone(); caches.open(CACHE).then(cache => cache.put(e.request, clone)); return res; });
+    }));
+  }
+});
