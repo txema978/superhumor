d9cab74ddf116c4e8e03e3024b1732394989f6e2
txema978 <txema978@gmail.com>
2025-08-21 22:43:10 +0200

    Add files via upload

--- env.js
+++ env.js
@@ -1,6 +1,4 @@
-<script>
-  window.ENV = {
-    SUPABASE_URL: 'https://qjmugmnrgxccliyrlwfz.supabase.co',
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqbXVnbW5yZ3hjY2xpeXJsd2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3ODQzOTIsImV4cCI6MjA3MTM2MDM5Mn0.ZMM9eNBj4HQxJqZ-RKDS8YLCL20xf3geyHnLm1kGUPo'
-  };
-</script>
+window.env = {
+  SUPABASE_URL: "https://qjmugmrgxccliyrlwfz.supabase.co",
+  SUPABASE_ANON_KEY: "TU_ANON_KEY_AQUI"
+};
