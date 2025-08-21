107936bd8af2964ce80b64e887dfab1ae64c5515
txema978 <txema978@gmail.com>
2025-08-21 21:43:03 +0200

    Add files via upload

---
+++ env.js
@@ -0,0 +1,6 @@
+<script>
+  window.ENV = {
+    SUPABASE_URL: 'https://qjmugmnrgxccliyrlwfz.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqbXVnbW5yZ3hjY2xpeXJsd2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3ODQzOTIsImV4cCI6MjA3MTM2MDM5Mn0.ZMM9eNBj4HQxJqZ-RKDS8YLCL20xf3geyHnLm1kGUPo'
+  };
+</script>
